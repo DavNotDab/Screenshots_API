2ec3cd56bbd643eb4822a4e64fcecafb352492b9
David Ballesteros Ortiz <david13ballesteros@gmail.com>
2022-11-13 18:10:04 +0100

    Initial commit

---
+++ scripts/drive.js
@@ -0,0 +1,39 @@
+import {google} from "googleapis";
+import got from 'got';
+
+// Auth details
+const CLIENT_ID = '13144220183-uq6q6n2rqj7dde8q972j5e3f8rsq2opi.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+
+const REFRESH_TOKEN = '1//04b_xmSXuqR6KCgYIARAAGAQSNwF-L9IrNlaG6zHPN2ONC6Bk1dusYQdSFzUYcQ7MP2WS0ye5uDAU5o7UZkSTPuAi36v2EHWo7OY';
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const drive = google.drive({
+    version: 'v3',
+    auth: oAuth2Client
+})
+
+// Function that uploads a file to Google Drive
+// Takes the file's URL, the file's name and the file's type as parameters
+// The file's type is optional and defaults to 'image/jpg'
+export async function uploadFile(url, name, type = 'image/jpg') {
+    try {
+        const response = await drive.files.create({
+            requestBody: {
+                name: name,
+                mimeType: type,
+                parents: ['1G2WJtMyf79-Voyl56K2sE1K0uwwqjxTi']
+            },
+            media: {
+                mimeType: type,
+                body: got.stream(url)
+            }
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+import screenshotmachine from 'screenshotmachine';
+import {uploadFile} from './drive.js';
+const customerKey = '545b8c';
+const secretPhrase = '';
+
+// Function that takes a site's URL and returns the site's screenshot URL
+function takeScreenshot(url) {
+    let options = {
+        url: url,
+        dimension: '1920x1080',
+        device: 'desktop',
+        format: 'jpg',
+        delay: '100',
+    }
+
+    return screenshotmachine.generateScreenshotApiUrl(customerKey, secretPhrase, options);
+}
+
+// Function that convert the sites' URLs into the sites' screenshots URLs
+// Takes an array of sites' urls as parameter and returns a same-length array with the sites' screenshots URLs
+function getApiUrls(urls) {
+    return urls.map(takeScreenshot);
+}
+
+// Function that gets the clean name of a site given its URL
+// Takes an array of sites' urls as parameter and returns a same-length array with the sites' clean names
+function getFileNames(urls) {
+    return urls.map(url => url.replace(/.+\/\/|www.|\..+/g, ''));
+}
+
+// Array with all the sites' URLs to be screenshotted
+const urls = [
+    "https://ifunded.de/en/",
+    "www.propertypartner.co",
+    "propertymoose.co.uk",
+    "www.homegrown.co.uk",
+    "https://www.realtymogul.com"
+];
+
+const screenshots = getApiUrls(urls);
+const names = getFileNames(urls);
+
+// Loop that uploads all the screenshots to Google Drive
+// It also logs any errors that may occur
+for (let i = 0; i < screenshots.length; i++) {
+    uploadFile(screenshots[i], `${i+1}_`+names[i]+'.jpg').then(() => {
+        console.log('File uploaded');
+    }).catch((error) => {
+        console.log(error.message);
+    });
+}
+
+// To see the uploaded files,
+// go to https://drive.google.com/drive/folders/1G2WJtMyf79-Voyl56K2sE1K0uwwqjxTi?usp=sharing
